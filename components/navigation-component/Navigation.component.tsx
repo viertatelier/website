ed7e5be223c7630a29bed1d804065d376399927b
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2024-08-29 22:27:11 -0300

    feat: Update navigation component with submenu functionality

--- components/navigation-component/Navigation.component.tsx
+++ components/navigation-component/Navigation.component.tsx
@@ -1,19 +1,66 @@
 import React from "react";
 import { navigation_data } from "@/data/navigation-data";
 import styles from "./Navigation.module.scss";
+import gsap from "gsap";
+import localFont from "next/font/local";
+
+const canelaFont = localFont({
+  src: "../../styles/fonts/canela/CanelaText-LightItalic-Trial.otf",
+});
+
 
 const Navigation: React.FC = () => {
+  const handleMouseEnter = (e: React.MouseEvent<HTMLLIElement>) => {
+    const submenu = e.currentTarget.querySelector("ul");
+
+    if (submenu) {
+      gsap.to(submenu, {
+        autoAlpha: 1,
+        y: 0,
+        duration: 0.3,
+        ease: "power3.out",
+      });
+      submenu.classList.add(styles.submenuVisible);
+    }
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent<HTMLLIElement>) => {
+    const submenu = e.currentTarget.querySelector("ul");
+
+    if (submenu) {
+      gsap.to(submenu, {
+        autoAlpha: 0,
+        y: -10,
+        duration: 0.3,
+        ease: "power3.inOut",
+      });
+      submenu.classList.remove(styles.submenuVisible);
+    }
+  };
+
   return (
     <nav>
       <ul className={styles.list}>
         {navigation_data.map((navigation) => (
-          <li key={navigation.id}>
-            <a
-              className="uppercase text-white"
-              href={navigation.url}
-            >
+          <li
+            key={navigation.id}
+            className={styles.listItem}
+            onMouseEnter={navigation.submenu ? handleMouseEnter : undefined}
+            onMouseLeave={navigation.submenu ? handleMouseLeave : undefined}
+            data-no-blobity
+          >
+            <a className="uppercase text-white" href={navigation.url} data-no-blobity>
               {navigation.title}
             </a>
+            {navigation.submenu && (
+              <ul className={styles.submenu}>
+                {navigation.submenu.map((subItem) => (
+                  <li key={subItem.id}  data-no-blobity>
+                    <a href={subItem.url}  data-no-blobity className={canelaFont.className}>{subItem.title}</a>
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
         ))}
       </ul>
--- data/navigation-data.tsx
+++ data/navigation-data.tsx
@@ -1,17 +1,35 @@
-export const navigation_data = [
-    {
+type NavigationDataProps = {
+  id: number;
+  title: string;
+  url?: string;
+  submenu?: NavigationDataProps[];
+};
+
+export const navigation_data: NavigationDataProps[] = [
+  {
+    id: 0,
+    title: "Quem somos",
+    url: "#about",
+  },
+  {
+    id: 1,
+    title: "Coleções",
+    submenu: [
+      {
         id: 0,
-        title: 'Home',
-        url: '/'
-    },
-    {
+        title: "Noivas Viert",
+        url: "/collection/noivas-viert",
+      },
+      {
         id: 1,
-        title: 'Coleções',
-        url: '/#collections'
-    },
-    {
-        id: 2,
-        title: 'Contato',
-        url: '#contact'
-    },
-]
+        title: "Festas Viert",
+        url: "/collection/festas-viert",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Contato",
+    url: "#contact",
+  },
+];
