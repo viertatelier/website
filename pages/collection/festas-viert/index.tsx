72a2695cf68b378a06cec10c332b1c8529c29c01
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2024-09-25 01:24:18 -0300

    Optimize image loading and remove console.log statement in festas-viert index.tsx

--- pages/collection/festas-viert/index.tsx
+++ pages/collection/festas-viert/index.tsx
@@ -52,7 +52,6 @@ export const getStaticProps = async () => {
       revalidate: 60 * 5, // 5 minutes
     };
   } catch (error) {
-    console.log(error);
     return {
       props: {
         insta: [],
